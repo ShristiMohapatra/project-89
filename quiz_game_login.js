e4f7dfde15e15c1b870fcd73073065866520422b
ShristiMohapatra <84072850+ShristiMohapatra@users.noreply.github.com>
2021-10-25 12:00:39 +0530

    Add files via upload

--- quiz_game_login.js
+++ quiz_game_login.js
@@ -1,3 +1,18 @@
+player1_name = localStorage.getItem("player1_name");
+player2_name = localStorage.getItem("player2_name");
+
+document.getElementById("player1_name").innerHTML=player1_name+" : ";
+document.getElementById("player2_name").innerHTML=player2_name+" : ";
+
+player1_score=0;
+player2_score=0;
+
+document.getElementById("player1_score").innerHTML=player1_score;
+document.getElementById("player2_score").innerHTML=player2_score;
+
+document.getElementById("player_question").innerHTML="Question Turn - "+player1_name;
+document.getElementById("player_answer").innerHTML="Answer Turn - "+player2_name;
+
 function addUser(){
     player1_name = document.getElementById("player1_input").value;
     player2_name = document.getElementById("player2_input").value;
@@ -6,4 +21,10 @@ function addUser(){
     localStorage.setItem("player2_name",player2_name);
 
     window.location = "quiz_game_page.html";
+}
+
+function send(){
+    number1 = document.getElementById("number1").value;
+    number2 = document.getElementById("number2").value;
+    actual_answer= parseInt(number1) * parseInt(number2);
 }
