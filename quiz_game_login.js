39a8fc138a46d2350aa2a9436fe14cd7d09828d0
ShristiMohapatra <84072850+ShristiMohapatra@users.noreply.github.com>
2021-10-26 12:39:06 +0530

    Add files via upload

--- quiz_game_login.js
+++ quiz_game_login.js
@@ -1,8 +1,8 @@
-player1_name = localStorage.getItem("player1_name");
-player2_name = localStorage.getItem("player2_name");
+player1_name = localStorage.getItem("player_1_name");
+player2_name = localStorage.getItem("player_2_name");
 
-document.getElementById("player1_name").innerHTML=player1_name+" : ";
-document.getElementById("player2_name").innerHTML=player2_name+" : ";
+document.getElementById("player_1_name").innerHTML = player1_name+" : ";
+document.getElementById("player_2_name").innerHTML = player2_name+" : ";
 
 player1_score=0;
 player2_score=0;
@@ -28,3 +28,5 @@ function send(){
     number2 = document.getElementById("number2").value;
     actual_answer= parseInt(number1) * parseInt(number2);
 }
+
+question_number = "<h4>"+number1+"X"+number2+"</h4>";
